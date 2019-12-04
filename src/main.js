5a966f68e0b48b35549abc7ee3a9d47c777db62d
ywhu <ywhu@in-road.com>
2019-12-04 11:12:55 +0800

    添加scss修改elementUI样式

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from './App'
 import router from './router'
 import axios from 'axios'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+import './styles/theme.scss'
 
 Vue.config.productionTip = false
 
